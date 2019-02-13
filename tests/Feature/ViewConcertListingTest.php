bc2b25b5642596d5f298c808186b03ceb8e365d6
vladkudinov89 <asutpdp3>
2019-02-13 10:19:11 +0300

    Add ViewConcertTest. Test OK.

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -17,4 +17,17 @@ class ViewConcertListingTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function test_for_travis()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $response->assertSee('Laravel');
+
+        $response->assertDontSee('Home2');
+    }
+
+
 }
