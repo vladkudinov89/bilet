e477a6eeaeb0df549aa0e5cc7afb767edb4d9d60
vladkudinov89 <asutpdp3>
2019-02-13 12:21:27 +0300

    Add endpoint Concert View. Must add dusk package to test.

--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -2,33 +2,43 @@
 
 namespace Tests\Feature;
 
+use App\Concert;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ViewConcertListingTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use DatabaseMigrations;
+
+    public function test_a_user_can_view_a_concert_listing()
     {
-        $this->assertTrue(true);
-    }
+        $concert = Concert::create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13, 2016 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555.'
 
-    public function test_for_travis()
-    {
-        $response = $this->get('/');
+        ]);
 
-        $response->assertStatus(200);
 
-        $response->assertSee('Laravel');
-
-        $response->assertDontSee('Home2');
-
-        $response->assertDontSee('Home3');
+        $this->visit('/concert/' . $concert->id)
+            ->assertSee('The Red Chord')
+            ->assertSee('with Animosity and Lethargy')
+            ->assertSee('December 13, 2016 8:00pm')
+            ->assertSee('32.50')
+            ->assertSee('The Mosh Pit')
+            ->assertSee('123 Example Lane')
+            ->assertSee('Laraville, ON 17916')
+            ->assertSee('For tickets, call (555) 555-5555.');
     }
 
 
