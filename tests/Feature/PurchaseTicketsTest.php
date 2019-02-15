c0f5a5911a839c5c18267e8a7c51fca8beca26ce
vladkudinov89 <asutpdp3>
2019-02-15 14:23:46 +0300

    Test required email when purchase tickets. Test OK.

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -12,26 +12,34 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PurchaseTicketsTest extends TestCase
 {
+    protected $paymentGateway;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->paymentGateway = new FakePaymentGateway();
+        $this->app->instance(PaymentGateway::class , $this->paymentGateway);
+    }
+
     use DatabaseMigrations;
+
 
     public function test_customer_can_purchase_concert_tickets()
     {
-        $paymentGateway = new FakePaymentGateway();
-        $this->app->instance(PaymentGateway::class , $paymentGateway);
-
         $concert = factory(Concert::class)->create([
-           'ticket_price' => 3250
+            'ticket_price' => 3250
         ]);
 
         $response = $this->json('post' , "/concert/{$concert->id}/orders" , [
             'email' => 'test@test.tu',
             'ticket_quantity' => 3,
-            'payment_token' => $paymentGateway->getValidTestToken()
+            'payment_token' => $this->paymentGateway->getValidTestToken()
         ]);
 
         $response->assertStatus(201);
 
-        $this->assertEquals(9750 , $paymentGateway->totalCharges());
+        $this->assertEquals(9750 , $this->paymentGateway->totalCharges());
 
 
         $order = $concert->orders()->where('email' , 'test@test.tu')->first();
@@ -40,5 +48,21 @@ class PurchaseTicketsTest extends TestCase
 
         $this->assertEquals(3 , $order->tickets()->count());
     }
+
+    public function test_email_is_required_to_pusrchase_tickets()
+    {
+        $concert = factory(Concert::class)->create();
+
+        $response = $this->json('post' , "/concert/{$concert->id}/orders" , [
+            'email' => '',
+            'ticket_quantity' => 3,
+            'payment_token' => $this->paymentGateway->getValidTestToken()
+        ]);
+
+        $response->assertStatus(422);
+
+        $this->assertArrayHasKey('email' , $response->json()['errors']);
+    }
+
 
 }
