7dc59174a4d8aa15a7e3e13124d2574cda8bcffc
vladkudinov89 <asutpdp3>
2019-02-20 12:06:34 +0300

    Testing real payment gateway. Test OK.

--- tests/Unit/Billing/StripePaymentTest.php
+++ tests/Unit/Billing/StripePaymentTest.php
@@ -9,28 +9,55 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class StripePaymentTest extends TestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->lastCharge = $this->lastCharge();
+    }
+
+    private function lastCharge()
+    {
+        return \Stripe\Charge::all(
+            ["limit" => 1],
+            ["api_key" => config('services.stripe.secret')]
+        )['data'][0];
+    }
+
+    private function validToken()
+    {
+        return \Stripe\Token::create([
+            "card" => [
+                "number" => '4242424242424242',
+                "exp_month" => 11,
+                "exp_year" => 2020,
+                "cvc" => "991"
+
+            ]
+        ], ['api_key' => config('services.stripe.secret')])->id;
+    }
+
+    private function newCharges()
+    {
+        return \Stripe\Charge::all(
+            [
+                "limit" => 1,
+                "ending_before" => $this->lastCharge->id
+            ],
+            ["api_key" => config('services.stripe.secret')]
+        )['data'];
+    }
+
     public function test_charges_with_a_valid_payment_token_are_successful()
     {
+
         $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
-        $token = \Stripe\Token::create([
-           "card" => [
-               "number" => '4242424242424242',
-               "exp_month" => 11,
-               "exp_year" => 2020,
-               "cvc" => "991"
+        $paymentGateway->charge(2500, $this->validToken());
 
-           ]
-        ] , ['api_key' => config('services.stripe.secret')])->id;
+        $this->assertCount(1, $this->newCharges());
 
-        $paymentGateway->charge(2500 , $token);
-
-       $lastCharge = \Stripe\Charge::all(
-           ["limit" => 1],
-           ["api_key" => config('services.stripe.secret')]
-       )['data'][0];
-
-        $this->assertEquals(2500 , $lastCharge->amount);
+        $this->assertEquals(2500, $this->lastCharge()->amount);
     }
 
 }
